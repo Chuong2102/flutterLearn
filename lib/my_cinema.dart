1761d6b644c3364b8b737098a25477bcb0e3883c
Chương Đoàn <chuongdoan2102@gmail.com>
2023-10-06 20:09:19 +0700

    nop bai slide cine

--- lib/my_cinema.dart
+++ lib/my_cinema.dart
@@ -1,12 +1,60 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class MyCinema extends StatelessWidget {
+class MyCinema extends StatefulWidget {
    MyCinema({super.key});
 
+
+  @override
+  State<StatefulWidget> createState() => _CreateState();
+}
+
+class _CreateState extends State<MyCinema> {
   List<String> popMenu = ["M.U", "ARS", "MANCUTE", "TLINH", "MCCHUONG"];
   String url0 = "https://m.media-amazon.com/images/M/MV5BOTkzYmMxNTItZDAxNC00NGM0LWIyODMtMWYzMzRkMjIyMTE1XkEyXkFqcGdeQXVyMTAyMjQ3NzQ1._V1_FMjpg_UX1000_.jpg";
   String url1 = "https://a.storyblok.com/f/178900/1080x1350/1844ab885f/one-piece-live-action-poster.jpg/m/filters:quality(95)format(webp)";
   String url2 = "https://posterspy.com/wp-content/uploads/2023/04/Picsart_23-04-18_06-52-22-362.jpg";
+
+  List<String> images = [
+    "https://m.media-amazon.com/images/M/MV5BOTkzYmMxNTItZDAxNC00NGM0LWIyODMtMWYzMzRkMjIyMTE1XkEyXkFqcGdeQXVyMTAyMjQ3NzQ1._V1_FMjpg_UX1000_.jpg",
+    "https://a.storyblok.com/f/178900/1080x1350/1844ab885f/one-piece-live-action-poster.jpg/m/filters:quality(95)format(webp)",
+    "https://posterspy.com/wp-content/uploads/2023/04/Picsart_23-04-18_06-52-22-362.jpg"
+  ];
+
+  ScrollController controller = ScrollController(initialScrollOffset: 0.0);
+  late Timer timer;
+  int curIndex = 0;
+
+  @override
+  void initState(){
+    super.initState();
+
+    controller.addListener(() {
+      var index = (controller.offset / 400).round();
+
+      if (index != curIndex) {
+        setState(() {
+          curIndex = index;
+        });
+      }
+    });
+
+    timer = Timer.periodic(const Duration(seconds: 4), (timer) {
+      setState(() {
+        curIndex = (curIndex + 1) % images.length; // Get index
+        moveImage();
+      });
+    });
+  }
+
+  void moveImage() {
+    controller.animateTo(
+      curIndex * MediaQuery.of(context).size.width,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.ease,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +63,6 @@ class MyCinema extends StatelessWidget {
         centerTitle: true,
         flexibleSpace: Container(
           decoration: const BoxDecoration(
-
               gradient: LinearGradient(
                   colors: [
                     Colors.black,
@@ -41,7 +88,7 @@ class MyCinema extends StatelessWidget {
         actions: [
           PopupMenuButton(
             itemBuilder: (context) {
-              return popMenu.map((e){
+              return popMenu.map((e) {
                 return PopupMenuItem(child: Text(e));
               }).toList();
             },
@@ -49,38 +96,66 @@ class MyCinema extends StatelessWidget {
         ],
       ),
       backgroundColor: Colors.white,
-      body: SizedBox(
-        width: double.infinity,
-        height: 300,
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: [
-            SizedBox(
-              width: 400,
-              height: 300,
-              child: Image(
-                  image: NetworkImage(url0),
-                  fit: BoxFit.contain,
-                ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.black54,
+                  width: 1
+                )
+              )
             ),
-            SizedBox(
-              width: 400,
-              height: 300,
-              child: Image(
-                image: NetworkImage(url1),
-                fit: BoxFit.contain,
-              ),
+            child: const Text(
+              "PHIM HOT",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blueGrey),
             ),
-            SizedBox(
-              width: 400,
-              height: 300,
-              child: Image(
-                image: NetworkImage(url2),
-                fit: BoxFit.contain,
-              ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.all(5),
+            height: 450,
+            decoration: const BoxDecoration(
+              color: Colors.black
             ),
-          ],
+            child: ListView.builder(
+              controller: controller,
+              scrollDirection: Axis.horizontal,
+              itemCount: images.length,
+              itemBuilder: (BuildContext context, int index){
+                return SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Image(
+                    image: NetworkImage(images[index]),
+                    fit: BoxFit.contain,
+                  ),
+                );
+              },
+            ),
         ),
+        Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: images.map((image) {
+                int index = images.indexOf(image);
+                return Container(
+                    width: 10,
+                    height: 10,
+                    margin: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: index == curIndex ? Colors.blue : Colors.grey
+                    )
+                );
+              }).toList() ,
+            )
+          ],
+        )
+      ]
       ),
     );
   }
